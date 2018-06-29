0848e25190de485c8d72baa72e45bc7df20cea87
Samuel Watson <otacon100@hotmail.com>
2018-06-29 12:39:34 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 var playlist = {Nero: "Promises"};
 
 function updatePlaylist(playlist, artistName, songtitle){
-  Object.assign (playlist, {Pendulum: 'Tarantula'})
+  playlist.pendulum = "tarantula";
   return playlist
 }
