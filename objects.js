c61a7ffa7e286d20b8b25dcbce05e049af7b85ee
Samuel Watson <otacon100@hotmail.com>
2018-06-29 12:54:07 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,6 +1,6 @@
 const playlist = new Object({ Nero: 'Promises'})
 
 function updatePlaylist(playlist, artistName, songtitle){
-  Object.assign({}, playlist, { ['pendulum']: 'tarantula'})
+  playlist['Pendulum'] = 'Tarantula'
   console.log(playlist)
 }
